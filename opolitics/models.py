a5e63fa2a852d3e2ab360a2e2dcb13065f3f6512
Justin Curl <jcurl@princeton.edu>
2020-03-31 01:04:40 -0700

    made iterable

--- opolitics/models.py
+++ opolitics/models.py
@@ -28,7 +28,7 @@ class Subsession(BaseSubsession):
         """Initializes the 
         session and creates the order in which the Blocks should be run through
         """
-        block_order = [BLOCKS[i].block_index for i in len(BLOCKS)]
+        block_order = [BLOCKS[i].block_index for i in range(len(BLOCKS))]
         self.block_order = json.dumps(block_order)
 
     def get_block_order(self) -> List[int]:
