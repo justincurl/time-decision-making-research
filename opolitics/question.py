6627dec3111841e140caaa10a2fa97e635c5b963
Justin Curl <jcurl@princeton.edu>
2020-03-31 00:45:55 -0700

    updated block_changes

--- opolitics/question.py
+++ opolitics/question.py
@@ -11,11 +11,12 @@ class Question:
     and of course the total available budget.
     """
 
-    def __init__(self, block: Block, index: int):
+    def __init__(self, block: Block, block_index: int index: int):
         """Create a new Question
         """
         self.block = block
         self.index = index
+        self.block_index
         self.num_choices = block.number_of_choices
         self.value = self.block.values[self.index]
 
@@ -24,12 +25,12 @@ class Question:
         
         :return: Question number
         """
-        return len(self.block.values)*self.block.block_index + self.index + 1
+        return len(self.block.values)*self.block_index + self.index + 1
 
     def start_values(self) -> List[str]:
         """ Take the initial number, and determine how much to decrease the amount by
             The expected number of values in this version is going to be 6. Decrease rate is 0.8 in this example.
-            These numbers will be decreasing
+            These numbers will be decreasing 
         
         :return: list of values
         """
